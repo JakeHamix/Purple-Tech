f5335be5ebae0b678dfaa829ebc0135d82876cb5
JakeHamix <jakubhamadej@gmail.com>
2023-08-21 20:49:06 +0200

    change icons

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CurrencyConverter from './CurrencyConverter';
 import NavbarComponent from './Navbar';
 import './App.css';
-
+// favicon provided by https://icons8.com/icons/set/favicon-money
 function App() {
   return (
     <div className="App">
